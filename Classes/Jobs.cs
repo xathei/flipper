46310bb100e2976eaec9781b3f325ff1eea6a645
dazusu <liam@officeirc.com>
2016-09-13 18:47:53 +0100

    Implemented base Jobs class, and example Thief override.

---
+++ Classes/Jobs.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FFACETools;
+
+namespace Flipper.Classes
+{
+    public class Jobs
+    {
+        private FFACE fface;
+
+        /// <summary>
+        /// The current event the player is participating in.
+        /// </summary>
+        public enum Content
+        {
+            Dynamis,
+            Farming,
+            Ambuscade,
+            Voidwatch,
+            Unity
+        }
+
+        /// <summary>
+        /// Called to attempt to make a claim on the current target immediately.
+        /// This will not be called once the target is claimed or is in range.
+        /// </summary>
+        /// <param name="content">The content the user is currently particpating in, to help you decisions.</param>
+        public virtual void UseClaim(Content content)
+        {
+
+        }
+
+        /// <summary>
+        /// Called continually in content where a monster needs staggering, and hasn't been staggered yet.
+        /// </summary>
+        /// <param name="content"></param>
+        public virtual void Stagger(Content content)
+        {
+            
+        }
+
+
+        /// <summary>
+        /// Called during battle.
+        /// </summary>
+        /// <param name="content">The content the user is currently particpating in, to help you decisions.</param>
+        public virtual void UseAbilities(Content content)
+        {
+            
+        }
+
+
+        /// <summary>
+        /// Called continually during battle.
+        /// </summary>
+        /// <param name="content">The content the user is currently particpating in, to help you decisions.</param>
+        public virtual void UseSpells(Content content)
+        {
+            
+        }
+
+        /// <summary>
+        /// Called continually with priority if the player's HP is below 75%.
+        /// </summary>
+        /// <param name="content">The content the user is currently particpating in, to help you decisions.</param>
+        public virtual void UseHeals(Content content)
+        {
+            
+        }
+
+
+        #region Helper Methods
+        public Dictionary<string, DateTime> CommandsLog = new Dictionary<string, DateTime>();
+        public DateTime NextCommandAllowed = DateTime.MinValue;
+
+
+        public void SendCommand(string command, int Delay = 2, bool PreventGlobal = false)
+        {
+            if ((!CommandsLog.ContainsKey(command) || (CommandsLog.ContainsKey(command) && CommandsLog[command] < DateTime.Now)) &&
+                (NextCommandAllowed <= DateTime.Now || PreventGlobal))
+            {
+                //WriteLog("BattleBot: Sending command ->" + command, true);
+
+                if (CommandsLog.ContainsKey(command))
+                    CommandsLog[command] = DateTime.Now.AddSeconds(Delay);
+                else
+                    CommandsLog.Add(command, DateTime.Now.AddSeconds(Delay));
+
+                if (!PreventGlobal)
+                    NextCommandAllowed = DateTime.Now.AddSeconds(Delay);
+                else
+                    NextCommandAllowed = DateTime.MinValue;
+
+                fface.Windower.SendString(command);
+            }
+        }
+
+        /// <summary>
+        /// Uses an ability from the AbilityList enum.
+        /// </summary>
+        /// <example>
+        ///     UseAbility(AbilityList.Provoke, 3, true);
+        ///     UseAbility(AbilityList.Sentinel, 3, false);
+        /// </example>
+        /// <param name="ability">The ability to use</param>
+        /// <param name="Delay">How long does it take to use this ability? (Consider lockout after use)</param>
+        /// <param name="Offensive">Is this ability used on me, or my target?</param>
+        public void UseAbility(AbilityList ability, int Delay = 2, bool Offensive = false)
+        {
+            SendCommand("/ja \"" + ability.ToString().Replace('_', ' ') + "\" " + (Offensive ? "<t>" : "<me>"), Delay);
+        }
+
+
+        /// <summary>
+        /// Uses an ability list that belongs to a group of abilities; such as a Pet ability, DNC Step, or DNC Waltz.
+        /// </summary>
+        /// <example>
+        ///     UseAbility("Predator Claws", AbilityList.Pet_commands, 3, true);
+        /// </example>
+        /// <param name="ability">The ability to use</param>
+        /// <param name="abilityGroup">The ability group to use from AbilityList</param>
+        /// <param name="Delay">How long does it take to use this ability? (Consider lockout after use)</param>
+        /// <param name="Offensive">Is this ability used on me, or my target?</param>
+        public void UseAbility(string ability, AbilityList abilityGroup, int Delay = 2, bool Offensive = false)
+        {
+            SendCommand("/ja \"" + ability.ToString().Replace('_', ' ') + "\" " + (Offensive ? "<t>" : "<me>"), Delay);
+        }
+
+
+        /// <summary>
+        /// Uses a spell from the SpellList enum.
+        /// </summary>
+        /// <example>
+        ///     UseSpell(SpellList.Flash, 3, true);
+        ///     UseSpell(SpellList.Stoneskin, 3, false);
+        /// </example>
+        /// <param name="ability">The ability to use</param>
+        /// <param name="Delay">How long does it take to use this ability? (Consider lockout after use)</param>
+        /// <param name="Offensive">Is this ability used on me, or my target?</param>
+        public void UseSpell(SpellList spell, int castTime, bool Offensive = false)
+        {
+            SendCommand("/ma \"" + spell.ToString().Replace('_', ' ') + "\" " + (Offensive ? "<t>" : "<me>"));
+        }
+
+
+        /// <summary>
+        /// Check if the provided spell is ready to use.
+        /// </summary>
+        /// <param name="spell"></param>
+        /// <returns></returns>
+        public bool Ready(SpellList spell)
+        {
+            if (fface.Timer.GetSpellRecast(spell) == 0)
+                return true;
+
+            return false;
+        }
+
+
+        /// <summary>
+        /// Check if the provided ability is ready to use.
+        /// </summary>
+        /// <param name="ability"></param>
+        /// <returns></returns>
+        public bool Ready(AbilityList ability)
+        {
+            if (fface.Timer.GetAbilityRecast(ability) == 0)// && fface.Player.HasAbility(ability))
+                return true;
+
+            return false;
+        }
+
+        /// <summary>
+        /// Determines if a player is afflicted with a certain effect.
+        /// </summary>
+        /// <param name="effect">The effect to check for</param>
+        /// <returns>True if afflicted, else false.</returns>
+        public bool IsAfflicted(StatusEffect effect)
+        {
+            foreach (StatusEffect status in fface.Player.StatusEffects)
+            {
+                if (status == effect)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        #endregion
+    }
+}
---
+++ Classes/Thief.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FFACETools;
+
+namespace Flipper.Classes
+{
+    public class Thief : Jobs
+    {
+        private FFACE fface;
+
+        public Thief(FFACE instance)
+        {
+            fface = instance;
+        }
+
+
+        public override void UseClaim(Content content)
+        {
+            // if we have access to warrior abilities, let's try them first.
+            if (fface.Player.SubJob == Job.WAR)
+            {
+                if (base.Ready(AbilityList.Provoke))
+                    base.UseAbility(AbilityList.Provoke, 2, true);
+            }
+
+            // then let's try bully
+            if (base.Ready(AbilityList.Bully) && fface.NPC.Distance(fface.Target.ID) < 6.9)
+                base.UseAbility(AbilityList.Bully, 2, true);
+
+        }
+
+
+        public override void Stagger(Content content)
+        {
+            
+        }
+
+        public override void UseAbilities(Content content)
+        {
+            // use flee on cool down, no idea why.
+            if (!IsAfflicted(StatusEffect.Flee) && Ready(AbilityList.Flee))
+                base.UseAbility(AbilityList.Flee, 2, false);
+
+            // check for war abilities
+            if (fface.Player.SubJob == Job.WAR)
+            {
+                // use warcry
+                if (!IsAfflicted(StatusEffect.Warcry) && Ready(AbilityList.Warcry))
+                    base.UseAbility(AbilityList.Warcry, 2, false);
+
+
+                if (Ready(AbilityList.Berserk))
+                    base.UseAbility(AbilityList.Berserk, 2, false);
+            }
+        }
+
+        public override void UseHeals(Content content)
+        {
+        }
+
+        public override void UseSpells(Content content)
+        {
+        }
+
+
+        #region Add other methods here that aren't overrides, such as calculating finishing moves, etc
+
+        #endregion
+    }
+}
