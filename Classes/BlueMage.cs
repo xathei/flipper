177830dcf42dd54121780c8029724ad339cb00d8
jorgevp <kapao69x5@gmail.com>
2016-09-15 19:01:22 -0400

    blu.cs thread

--- Classes/BlueMage.cs
+++ Classes/BlueMage.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Flipper.Classes
@@ -13,6 +14,13 @@ namespace Flipper.Classes
         {
             _content = content;
             _fface = instance;
+        }
+
+        public override void UseRangedClaim()
+        {
+            _fface.Navigator.FaceHeading(_fface.Target.ID);
+            Thread.Sleep(1000);
+            SendCommand("/ra <t>", 5);
         }
 
         public override void UseAbilities()
