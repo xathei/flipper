f08532c8ebef71a8f6e4debed984d7ac4ef6bc5e
dazusu <liam@officeirc.com>
2016-09-18 16:47:33 +0100

    Added example of passing data to form.

--- Classes/Thief.cs
+++ Classes/Thief.cs
@@ -14,7 +14,7 @@ namespace Flipper.Classes
     public class Thief : Jobs
     {
         private int _bullyTimer;
-        private Form settingsForm = new ThiefForm();
+        private ThiefForm settingsForm = new ThiefForm();
 
 
         public Thief(FFACE instance, Content content)
@@ -26,6 +26,7 @@ namespace Flipper.Classes
         public override void SettingsForm()
         {
             settingsForm.Show();
+            settingsForm.PopulateUI("derp");
         }
 
         public override void UseRangedClaim()
